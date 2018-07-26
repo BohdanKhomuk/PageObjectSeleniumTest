beb6da81fe5b2d73ef2f36f985765da0a70fef55
bohdan.khomuk <bohdankhomuk@gmail.com>
2018-07-26 11:34:43 +0300

    My Test

---
+++ src/test/java/MainClass.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class MainClass {
+    //ppp
+    static WebDriver driver;
+    public static void main(String[] args){
+
+        String browser = new File( MainClass.class.getResource("/chromedriver.exe").getFile()).getPath();
+        System.setProperty("webdriver.chrome.driver", browser);
+
+        driver = new ChromeDriver(  );
+        driver.manage().timeouts().implicitlyWait( 5, TimeUnit.SECONDS );
+        driver.manage().window().maximize();
+        driver.get("http://github.com");
+        MainPage mainPage = new MainPage( driver );
+
+        mainPage.register( "testusername", "qweqw@xpr.com", "qwerty123" );
+    }
+}
